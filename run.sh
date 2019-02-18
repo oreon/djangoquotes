4f44bc3b3a1b1619962cad0e4c3b35556e83f786
oreon <singhjess@gmail.com>
2019-02-18 13:52:53 -0500

    added run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ source dquotes/bin/activate
 pip install -r requirements.txt  # all requirements - gunicorn djang$
 pip install gunicorn psycopg2-binary
 
-python src/manage.py migrate
-python src/manage.py collectstatic --noinput
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py installtasks
 sudo systemctl restart dquotes
 deactivate
