3d35b4a83d47f81a035d4f6de87616d1da881144
oreon <singhjess@gmail.com>
2019-04-27 14:05:04 -0400

    changes for info

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,7 @@ from .models import Order
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-import weasyprint
+#import weasyprint
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -29,15 +29,15 @@ def admin_order_detail(request, order_id):
                   'admin/orders/order/detail.html',
                   {'order': order})
 
-
-@staff_member_required
-def admin_order_pdf(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    html = render_to_string('orders/order/pdf.html',
-                            {'order': order})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'filename=order_{}.pdf"'.format(order.id)
-    weasyprint.HTML(string=html).write_pdf(response,
-                                           stylesheets=[weasyprint.CSS(
-                                               settings.STATIC_ROOT + 'css/pdf.css')])
-    return response
+#
+# @staff_member_required
+# def admin_order_pdf(request, order_id):
+#     order = get_object_or_404(Order, id=order_id)
+#     html = render_to_string('orders/order/pdf.html',
+#                             {'order': order})
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'filename=order_{}.pdf"'.format(order.id)
+#     weasyprint.HTML(string=html).write_pdf(response,
+#                                            stylesheets=[weasyprint.CSS(
+#                                                settings.STATIC_ROOT + 'css/pdf.css')])
+#     return response
